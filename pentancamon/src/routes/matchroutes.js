b1a1285d83c3737043783837ea4cdb4fd42d914b
jopen <solimonra@gmail.com>
2025-10-20 10:14:29 +0200

    ultima actualizacion

--- pentancamon/src/routes/matchroutes.js
+++ pentancamon/src/routes/matchroutes.js
@@ -3,17 +3,19 @@ import { Router } from 'express'
 import { matchCreateValidation } from '../validation/CreateMatValidation.js'
 import { HttpError } from '../utils/httpError.js'
 import { Match } from '../models/Match.js'
-import { createMatch, validDataMatched } from '../services/matches.js'
+import { createMatch, deleteMatch, MatchbyId, validDataMatched } from '../services/matches.js'
 import { isLoggedIn } from '../middlewares/loggedinout.js'
 import { User } from '../models/user.js'
 import mongoose from 'mongoose'
-
+import { matchbyId } from '../services/matches.js'
+import { joinAMatch } from '../services/matches.js'
+import { isOwner } from '../services/matches.js'
 
 const router = express.Router()
 
 
-router.post("/createMatch", async (req, res, next) => {
-  try {
+router.post("/createMatch", isLoggedIn, async (req, res, next) => {
+    try {
     const newMatch = await createMatch (req.user, req.body)
   
     console.log(newMatch)
@@ -23,25 +25,27 @@ router.post("/createMatch", async (req, res, next) => {
         if (err instanceof HttpError) {
     
             next(err);
-        }}})
+        }
+    }
+})
         
     //errores importantes: err.name, err.code
     // if (err.code === 11000) { return next(new HttpError(409, "Duplicate key error")
     // if (err.name === "ValidationError") {return next(new HttpError(400, "Validation error creating match"))}
 
 
-
 //RUTA LISTA DE PARTIDOS
 
 router.get("/matches", async (req, res, next) => {
-    const matches = await Match.find()
-        try {
-            if (!matches) {
-                throw new HttpError(404, "matches not found")
-            }
-            res.status(200).json(matches);
-        } catch (err) {
-            next(err)
+    try {
+        const matches = await Match.find()
+    
+        if (!matches) {
+            throw new HttpError(404, "matches not found")
+        }
+        res.status(200).json(matches);
+    } catch (err) {
+        next(err)
         }
     }
     )
@@ -49,19 +53,15 @@ router.get("/matches", async (req, res, next) => {
 //RUTA DETALLES DEL PARTIDO
 
 router.get("/matches/:matchId", isLoggedIn, async (req, res, next) => {
-    const { matchId } = req.params
+    try {
+        
+        const match = await matchbyId (req.params)
+        res.status(200).json(match);
 
-    const match = await Match.findById(matchId)
-        try {
-            if (!match) {
-                throw new HttpError(404, "match not found in DB")
-            }
-        res.status(200).json(match)
-        } catch(err) {
+    } catch(err) {
             next(err instanceof HttpError ? err : new HttpError(500, "Internal server error while fetching match"))
         }
-    }
-)
+    })
 
 
 
@@ -70,45 +70,8 @@ router.get("/matches/:matchId", isLoggedIn, async (req, res, next) => {
 
 router.post("/matches/:matchId/join", isLoggedIn, async (req, res, next) => {
     try {
-    const { matchId } = req.params
-    //necesaria la validacion con zod?
-    //const { _id: userId } = req.user
-    console.log("aqui el req.session.passport.user", req.session.passport.user)
-    console.log("aqui el req.user", req.user)
-    const {_id: userId } = req.user
 
-    console.log("id del match", matchId)
-    console.log("id del usuario", userId)
-    
-    //comprobar nivel de usuario y de match
-    const user = await User.findById (userId)
-    const match = await Match.findById(matchId)
-
-    console.log("objeto de usuario:", user)
-    console.log("objeto de match:", match)
-
-    //comprobar si existe match
-    if (!match) throw new HttpError(404, "Match not found")
-
-    //comprobar si el match esta completo
-    const currentPlayers = match.players.length
-    if (currentPlayers === match.maxPlayers) {
-        throw new HttpError(409, "Match is full")
-    }
-    //comprobar el nivel
-    if (user.level != match.level) {
-        throw new HttpError(403, "level not allowed to join")
-    }
-
-    //añado player al array de players
-    const matchUpdated = await Match.findOneAndUpdate( //con este devuelve documento .select (elijo los documentos que quiero traer)
-        { _id: matchId, players: {$ne: userId} }, //busca partido que no tenga a este player. //error indicando si no se cumple
-        {$addToSet: {players: userId}},
-        { new: true } //devuelva el actualizado
-    ).select("players")
-    if (!matchUpdated) {
-        throw new HttpError(500, "Error adding de player")
-    }
+    const matchUpdated = await joinAMatch(req.params, req.user)    
 
     console.log("player añadido", matchUpdated)
 
@@ -117,61 +80,34 @@ router.post("/matches/:matchId/join", isLoggedIn, async (req, res, next) => {
         playersCount: matchUpdated.players.length
     })
 
-
-} catch (err) {
-    next(err instanceof HttpError ? err : new HttpError (500, "Error joining the match"))
-}
+    } catch (err) {
+        next(err instanceof HttpError ? err : new HttpError (500, "Error joining the match"))
+    }
 }
 )
 
 //RUTA DELETE MATCH
 
-router.get("/match/:matchId/delete", async (req, res, next) => {
-    const { matchId } = req.params;
-    const { _id: userId } = req.user
-
-    //1) comprobar si es el owner con userId (en User DB)
-
-    const resultUser = await User.findById(userId)
+router.get("/matches/:matchId/delete", isLoggedIn, isOwner, async (req, res, next) => {
     try {
-    if (!resultUser) {
-        throw new HttpError(404, "Error finding user in DB")
-    }
+    
+        const deletedMatch = await deleteMatch(req.params, req.user)
+       
     } catch (err) {
-        next(err)
+        next(err instanceof HttpError ? err : new HttpError (500, "Error joining the match"))
     }
-
-    const matchOwner = await Match.findById(matchId) 
-    try {
-        if (!matchOwner) {
-        throw new HttpError(404, "Error finding match in DB") 
-    }
-    } catch (err) {
-        next(err)
-    }
-    console.log("userid", userId)
-    console.log("match owner id", matchOwner.creator)
-
-
-    if (userId.String() !== matchOwner.creator.String()) {
-        throw new HttpError(403, "User logged is not the owner")
-    }
-
-    //2) en Match findbyIdandDelete (con matchId)
-
-    const Deleted = await Match.findByIdAndDelete(matchId) 
-    try {
-    console.log("elemento eliminado")
-    if (!Deleted) {
-        throw new HttpError(500, "Error deleting match")
-    }
-    } catch (err) {
-        next(err)
-    }
-
-} )
+    } 
+)
 
 // RUTA UPDATE A MATCH
+
+router.post("/matches/:matchId/update", isLoggedIn, isOwner, (req, res, next) => {
+
+
+}
+
+
+)
 
 
 
--- pentancamon/src/services/matches.js
+++ pentancamon/src/services/matches.js
@@ -2,7 +2,11 @@ import { z } from 'zod'
 import { matchCreateValidation } from '../validation/CreateMatValidation.js'
 import { HttpError } from '../utils/httpError.js'
 import { Match } from '../models/Match.js'
+import { User } from '../models/user.js'
 
+
+
+//Funcion validar info de req.body (crear partido)
 export function validDataMatched (data) {
         const result = matchCreateValidation.safeParse(data)
         if (!result.success) {
@@ -11,7 +15,7 @@ export function validDataMatched (data) {
         } return result.data
     }
 
-
+//Funcion crear partida
 export async function createMatch (user, body) {
 
         console.log("req body aqui:", body)
@@ -39,4 +43,100 @@ export async function createMatch (user, body) {
         
         return newMatch 
 
+}
+
+//Funcion buscar partido por Id
+export async function matchbyId (dataParams) {
+    const { matchId } = dataParams
+    const match = await Match.findById(matchId)
+        if (!match) {
+            throw new HttpError(404, "match not found in DB")
+        }
+        return match
+}
+
+//Funcion unirte a un partido
+export async function joinAMatch (dataParams, dataUser) {
+        const { matchId } = dataParams
+        //necesaria la validacion con zod?
+        //const { _id: userId } = req.user
+        
+        console.log("aqui el req.user", dataUser)
+        const {_id: userId } = dataUser
+    
+        console.log("id del match", matchId)
+        console.log("id del usuario", userId)
+        
+        //comprobar nivel de usuario y de match
+        const user = await User.findById (userId)
+
+        const match = matchbyId (dataParams)
+
+        console.log("objeto de usuario:", user)
+        console.log("objeto de match:", match)
+    
+        //comprobar si el match esta completo
+        const currentPlayers = match.players.length
+        if (currentPlayers === match.maxPlayers) {
+            throw new HttpError(409, "Match is full")
+        }
+        //comprobar el nivel
+        if (user.level != match.level) {
+            throw new HttpError(403, "level not allowed to join")
+        }
+    
+        //añado player al array de players
+        const matchUpdated = await Match.findOneAndUpdate( //con este devuelve documento .select (elijo los documentos que quiero traer)
+            { _id: matchId, players: {$ne: userId} }, //busca partido que no tenga a este player. //error indicando si no se cumple
+            {$addToSet: {players: userId}},
+            { new: true } //devuelva el actualizado
+        ).select("players")
+        if (!matchUpdated) {
+            throw new HttpError(500, "Error adding de player")
+        }
+        return matchUpdated
+}  
+
+//Funcion borrar partido
+
+export async function deleteMatch (dataParams, dataUser) {
+        const { matchId } = dataParams;
+        const { _id: userId } = dataUser;
+
+        //1) comprobar si es el owner con userId (en User DB)
+
+
+        //2) en Match findbyIdandDelete (con matchId)
+
+        const deletedMatch = await Match.findByIdAndDelete(matchId) 
+            console.log("elemento eliminado")
+            if (!deletedMatch) {
+                throw new HttpError(500, "Error deleting match")
+            }
+        return deletedMatch
+}
+
+
+//Funcion comprobar si es el owner
+
+export async function isOwner (matchId, userId) {
+    const resultUser = await User.findById(userId)
+        
+        if (!resultUser) {
+            throw new HttpError(404, "Error finding user in DB")
+        }   
+
+        const matchOwner = await Match.findById(matchId) 
+        
+            if (!matchOwner) {
+            throw new HttpError(404, "Error finding match in DB") 
+        }
+        
+        console.log("userid", userId)
+        console.log("match owner id", matchOwner.creator)
+
+
+        if (userId.String() !== matchOwner.creator.String()) {
+            throw new HttpError(403, "User logged is not the owner")
+        }
 }
